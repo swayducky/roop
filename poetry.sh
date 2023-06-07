fc6bd52309c0e377cd8b7e9f17295656f81f08a4
Sway Ducky <swayducky@gmail.com>
2023-06-06 18:25:25 -0700

    simplify poetry.sh

--- poetry.sh
+++ poetry.sh
@@ -1,8 +1,9 @@
-source ~/.pyenv/versions/miniconda3-latest/bin/activate roop
+# source ~/.pyenv/versions/miniconda3-latest/bin/activate roop
 
-export PATH=/usr/local/cuda/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-export CUDA_HOME=/usr/local/cuda
+####### WORKS GREAT, but not needed
+# export PATH=/usr/local/cuda/bin:$PATH
+# export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+# export CUDA_HOME=/usr/local/cuda
 
 # export PATH=$HOME/local/cuda-11.7/bin:$PATH
 # export LD_LIBRARY_PATH=$HOME/local/cuda-11.7/lib64:$LD_LIBRARY_PATH
@@ -11,9 +12,11 @@ export CUDA_HOME=/usr/local/cuda
 # https://www.tensorflow.org/install/pip#windows-wsl2
 # export CUDNN_PATH=$HOME/c/roop/.venv/lib/python3.10/site-packages/nvidia/cudnn
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/c/roop/.venv/lib:$CUDNN_PATH/lib
-CUDNN_PATH=$(dirname $(poetry run python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
-echo "CUDNN_PATH: $CUDNN_PATH"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDNN_PATH/lib
+
+##### WORKS GREAT, but not needed
+# CUDNN_PATH=$(dirname $(poetry run python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
+# echo "CUDNN_PATH: $CUDNN_PATH"
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDNN_PATH/lib
 
 if [ $# -eq 0 ]
 then
